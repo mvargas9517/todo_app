e971ee3439a70812c30114794f05d52b570a3c4e
Macen Vargas <mvargas9517@live.com>
2020-04-16 23:48:26 -0500

    added db connection

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:todoapp/screens/home_screen.dart';
+import 'package:todoapp/services/category_service.dart';
+import 'package:todoapp/models/category.dart';
 
 class CategoriesScreen extends StatefulWidget {
   @override
@@ -10,6 +12,31 @@ class _CategoriesScreenState extends State<CategoriesScreen> {
 
   var _categoryName = TextEditingController();
   var _categoryDescription = TextEditingController();
+
+  var _category = Category();
+  var _categoryService = CategoryService();
+
+  List<Category> _categoryList = List<Category>();
+
+  @override
+  void initState() {
+    super.initState();
+    getAllCategories();
+  }
+
+  getAllCategories() async {
+    var categories = await _categoryService.getCategories();
+    categories.forEach((category) {
+    setState(() {
+      var model = Category();
+      model.name = category['name'];
+      _categoryList.add(model);
+    });
+
+
+    }
+    );
+  }
 
   _showFormInDialog(BuildContext context) {
     return showDialog(context: context, barrierDismissible: true, builder: (param) {
@@ -22,9 +49,11 @@ class _CategoriesScreenState extends State<CategoriesScreen> {
             child: Text('Cancel'),
           ),
           FlatButton(
-            onPressed: () {
-//              print('Category Name' : $_categoryName);
-//              print('Category Description' : $_categoryDescription);
+            onPressed: () async {
+              _category.name = _categoryName.text;
+              _category.description = _categoryDescription.text;
+            var result = await _categoryService.saveCategory(_category);
+            print(result);
             },
             child: Text('Save'),
           ),
@@ -55,19 +84,35 @@ class _CategoriesScreenState extends State<CategoriesScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: RaisedButton(
-          elevation: 0.0,
-          color: Colors.red,
-          onPressed: () {
-            Navigator.of(context).push(new MaterialPageRoute(builder: (context) => new HomeScreen()));
-          },
-          child: Icon(Icons.arrow_back, color: Colors.white),
+        appBar: AppBar(
+          leading: RaisedButton(
+            elevation: 0.0,
+            color: Colors.red,
+            onPressed: () {
+              Navigator.of(context).push(new MaterialPageRoute(
+                  builder: (context) => new HomeScreen()));
+            },
+            child: Icon(Icons.arrow_back, color: Colors.white),
+          ),
+          title: Text('Todo app'),
         ),
-        title: Text('Todo app'),
-      ),
-      body: Center(child: Text('Welcome to categories screen'),),
+        body: ListView.builder(
+            itemCount: _categoryList.length, itemBuilder: (context, index) {
+          return Card(child: ListTile(
+            leading: Icon(Icons.edit),
+            onTap: () {},
+            title: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(_categoryList[index].name),
+                IconButton(
+                  icon: Icon(Icons.delete),
+                  onPressed: () {},)
+              ],
+            ),));
+        }),
+
       floatingActionButton: FloatingActionButton(onPressed: () {_showFormInDialog(context);}, child: Icon(Icons.add),),
-    );
+  );
   }
 }
---
+++ lib/services/category_service.dart
@@ -0,0 +1,19 @@
+import 'package:todoapp/models/category.dart';
+import 'package:todoapp/repo/repository.dart';
+
+class CategoryService{
+
+  Repository _repository;
+
+  CategoryService () {
+    _repository = Repository();
+  }
+
+  saveCategory(Category category) async {
+    return await _repository.save('categories', category.categoryMap());
+  }
+
+  getCategories() async {
+    return await _repository.getAll('categories');
+  }
+}
